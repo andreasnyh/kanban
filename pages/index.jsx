61cd753814455f1ac677e2dc3b20dacfdd65393c
Andreas Ny <andreas.nyh87@gmail.com>
2022-07-14 10:12:07 +0200

    Set up basic cols and tasks

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,34 @@
-
 export default function Home() {
-  return (
-    <div className="">
-      <main className="">
+  const cols = ["Todo", "Doing", "Done"];
+  const tasks = {
+    Todo: [
+      { name: "To be done", order: 0 },
+      { name: "To be done 2", order: 1 },
+    ],
+    Doing: [
+      { name: "Doing", order: 0 },
+      { name: "Doing 2", order: 1 },
+    ],
+    Done: [
+      { name: "Done", order: 0 },
+      { name: "Done 2", order: 1 },
+    ],
+  };
 
+  return (
+    <div className="bg-white h-screen max-h-screen overflow-hidden p-8">
+      <main className="min-h-full overflow-auto grid grid-flow-col auto-cols-[minmax(33.33vw,_1fr)] gap-4">
+        {cols &&
+          cols.map((col) => (
+            <div className="flex p-2 h-full grow basis-auto flex-col bg-slate-300 gap-4 drop-shadow-md">
+              <h2 className="text-lg">{col}</h2>
+              {tasks &&
+                tasks[col]?.map((task) => (
+                  <div className="card bg-stone-200 p-2">{task.name}</div>
+                ))}
+            </div>
+          ))}
       </main>
     </div>
-  )
+  );
 }
